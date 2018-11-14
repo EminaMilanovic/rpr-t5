2e88f50e38c06531c10dd1bb7b2739557d27fdaf
emilanovic1 <emilanovic1@etf.unsa.ba>
2018-11-14 23:09:22 +0100

    dodana fja plus

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,33 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-public class Controller {
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
+public class Controller {
+    float x;
+
+    @FXML
+    private Label display;
+
+    private SimpleStringProperty ekran;
+        public void fja1(ActionEvent actionEvent) {
+        display.setText(display.getText()+"1");
+    }
+
+    public void fja2(ActionEvent actionEvent) {
+        display.setText(display.getText()+"2");
+    }
+
+    public void fjaplus(ActionEvent actionEvent) {
+         x=Float.parseFloat(display.getText());
+            display.setText("");
+    }
+
+    public void fjaeq(ActionEvent actionEvent) {
+        x+=Float.parseFloat(display.getText());
+        display.setText(Float.toString(x));
+    }
 }
